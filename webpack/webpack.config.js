4be7e6f74d50485dc46d531a0df2f84fdea7b246
Kyran Butler <kyran@butlerhq.com>
2022-02-22 14:13:12 -0700

    Removed entry entirely from Webpack Config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
